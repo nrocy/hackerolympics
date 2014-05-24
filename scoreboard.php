66b6a08893342280c4d5b422aa8f8f1a2138d55f
Sven Steinbauer <sven@unlogic.co.uk>
2014-05-24 14:36:08 +0100

    Sort results

--- scoreboard.php
+++ scoreboard.php
@@ -4,5 +4,12 @@ $text = file_get_contents('http://hacker-olympics-london.herokuapp.com/leaderboa
 
 $json = json_decode($text);
 
+$scores = [];
+foreach( $json as $row ) {
+  $scores[] = array($row->name, $row->points);
+}
 
+usort($scores, function($a, $b) {
+  return $a[1] < $b[1];
+});
 
